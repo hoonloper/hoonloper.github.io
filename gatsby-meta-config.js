3eb7905a427196b41be9ad42402d695581c89a5b
Yonghoon <yonghoonj98@gmail.com>
2022-08-20 16:40:10 +0900

    블로그 템플릿 초기 세팅

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `hoonloper.com`,
+  description: `Hoonloper의 발자취 기록`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://www.hoonloper.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `정용훈`,
     bio: {
-      role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      role: `Back-End 개발자`,
+      description: ['소통을 중시하는', '새로운 도전을 즐기는', '성장에 목말라있는'],
+      thumbnail: 'me.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/hoonloper`,
+      linkedIn: ``,
+      email: `yonghoonj98@gmail.com`,
     },
   },
 
