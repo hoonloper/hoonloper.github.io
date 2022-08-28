215628f5dd3a717429f6d03fc7480147b91ad3f2
Yonghoon <yonghoonj98@gmail.com>
2022-08-28 21:37:25 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d80ddf8c73799bd63d6f.js"
+    "url": "webpack-runtime-7566e6d6adc3cac2ffcd.js"
   },
   {
-    "url": "styles.853e65090dc89bfbb4ac.css"
+    "url": "styles.c75940ba2e9d08c2bb15.css"
   },
   {
-    "url": "framework-043f1197be387cb18d15.js"
+    "url": "framework-852e141d9c20c74cfc74.js"
   },
   {
-    "url": "app-735b6b866e8b1d7bac84.js"
+    "url": "app-78149243d6b81d710e6c.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "622c62f4dee535220c039e724709fa82"
+    "revision": "09f19f21d55506924a441279883fd999"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -52,10 +52,7 @@ self.__precacheManifest = [
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fBBc4.woff2"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ffdb1e83dd2925d16ce3.js"
-  },
-  {
-    "url": "polyfill-9b356b5dc44213e24f34.js"
+    "url": "polyfill-29b51f959b53d5ab826b.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -169,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-735b6b866e8b1d7bac84.js`))) {
+  if (!resources || !(await caches.match(`/app-78149243d6b81d710e6c.js`))) {
     return await fetch(event.request)
   }
 
